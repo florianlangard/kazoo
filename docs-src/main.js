bdb9044983133f909530982e1b2fd08dbd49e02f
florianlangard <80707190+florianlangard@users.noreply.github.com>
2025-08-09 09:38:21 +0200

    github pages ready

--- docs-src/main.js
+++ docs-src/main.js
@@ -1,9 +1,7 @@
+import { translations } from './translations.js';
 import confetti from 'canvas-confetti';
 import 'notyf/notyf.min.css';
-
 import { Notyf } from 'notyf';
-
-console.log('main.js');
 
 const notyf = new Notyf({
   duration: 2000,
@@ -12,12 +10,23 @@ const notyf = new Notyf({
 const owner = 'florianlangard';
 const repo = 'kazoo';
 
-let downloadButton = document.getElementById('downloadBtn');
-let title = document.querySelector('h1');
+const langSelector = document.getElementById("langSelector");
+const savedLang = localStorage.getItem("lang") || "en";
+langSelector.value = savedLang;
 
+const downloadButton = document.getElementById('downloadBtn');
+const title = document.querySelector('h1');
+
+// EventListeners
 downloadButton.addEventListener('click', triggerConfetti);
 title.addEventListener('click', triggerConfetti);
 
+langSelector.addEventListener("change", (e) => {
+  setLanguage(e.target.value);
+  toggleLangNotice();
+});
+
+// version handling
 async function getLatestRelease() {
   try {
     const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/releases/latest`);
@@ -44,7 +53,30 @@ async function getLatestRelease() {
   }
 }
 
-// getLatestRelease();
+// Language handling
+function setLanguage(lang) {
+  document.querySelectorAll("[data-i18n]").forEach(el => {
+    const key = el.getAttribute("data-i18n");
+    el.textContent = translations[lang][key] || key;
+  });
+  localStorage.setItem("lang", lang);
+}
+
+function toggleLangNotice() {
+  if (localStorage.getItem('lang') === "en") {
+    let element = document.createElement('p');
+    element.className = "lang-notice";
+    let parent = document.querySelector('.description')
+    element.textContent = "This app is only avilable in French for now.";
+    parent.appendChild(element);
+  }
+  else {
+    let element = document.querySelector('.lang-notice');
+    if (element) {
+      element.remove();
+    }
+  }
+}
 
 // confetti =====
 function triggerConfetti() 
@@ -55,15 +87,23 @@ function triggerConfetti()
       angle: 120,
       spread: 50,
       origin: { x: 1 },
-      colors: ["#fff", "#0000ff"],
+      colors: ["#fff", "#b11414"],
     });
     confetti({
       particleCount: 80,
       angle: 60,
       spread: 50,
       origin: { x: 0 },
-      colors: ["#fff", "#b11414"],
+      colors: ["#fff", "#00f"],
     });
   }
   frame();
 }
+
+
+// functions calls
+document.addEventListener('DOMContentLoaded', () => {
+  toggleLangNotice();
+  setLanguage(savedLang);
+  getLatestRelease();
+});
---
+++ docs-src/translations.js
@@ -0,0 +1,14 @@
+export const translations = {
+  fr: {
+    description: "Tableau d'affichage pour matchs de théâtre d'improvisation.",
+    download: "Télécharger",
+    github: "Voir sur Github",
+    smartscreen: "Note : Cette application n’est pas signée numériquement. Lors de l’installation, Windows peut afficher un avertissement SmartScreen. Pour continuer, cliquez sur « Informations complémentaires », puis sur « Exécuter quand même »."
+  },
+  en: {
+    description: "A digital scoreboard for theatrical improv competitions. It lets you display team scores in real time — perfect for live performances.",
+    github: "See on Github",
+    download: "Download app",
+    smartscreen: "Note: This application is not digitally signed. When installing, Windows may show a SmartScreen warning. To continue, click 'More info' and then 'Run anyway'."
+  }
+};
